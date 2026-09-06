153db1024fd409734dd3176bd8950c7be086b2fa
Jorik <vanveenjorik@protonmail.com>
2026-09-06 14:22:53 +0200

    bump version

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-      version = "0.1.1";
+      version = "0.1.2";
     in
     {
       devShell = pkgs.mkShell {
